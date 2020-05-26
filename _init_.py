2d59890f2e66b57ce3d39f658daabb7cae34688d
Moon <kunsik11@gmail.com>
2020-05-26 16:28:38 +0900

    test2
    test2

--- _init_.py
+++ _init_.py
@@ -5,7 +5,7 @@ bot = telebot.TeleBot("1117988587:AAERFRl23gsQ6rOqcyeO4nSWpPWGdz_1Bh0")
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(message, """Welcome to Simpleatcity!
-    
+
     Before I can start recommending simple dishes for you,
     if you want better-tailored dishes, please head on to
     /fridge and /utensils to fill out the current state of
@@ -14,5 +14,6 @@ def send_welcome(message):
     main ingredients that you have, head on to /quickrecipe.
     For a better overview of the functionalities that this
     bot provides, use /help.""")
-
 bot.polling()
+
+#cool
