bda64f7b51555bcb1bb27b8ac1c506a87b129599
Moon <gsmoon97@gmail.com>
2020-07-16 12:58:30 +0900

    initial commit

--- __init__.py
+++ __init__.py
@@ -1,3 +1,5 @@
+import os
+from flask import Flask, request
 import telebot
 from telebot import types
 import inspect
@@ -8,6 +10,7 @@ import random
 
 TOKEN = '1117988587:AAERFRl23gsQ6rOqcyeO4nSWpPWGdz_1Bh0'
 bot = telebot.TeleBot(TOKEN)
+server = Flask(__name__)
 dbhelper.cache_ingredients()
 strlst = []
 # print(Cache.ingred_dict)
@@ -490,4 +493,20 @@ def get_random_recipe_str(input, idx):
 
 bot.enable_save_next_step_handlers(delay=2)
 bot.load_next_step_handlers()
-bot.polling()
+# bot.polling()
+
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://your_heroku_project.com/' + TOKEN)
+    return "!", 200
+
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
